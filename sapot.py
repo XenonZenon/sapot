90ba160d806e4fa47a161e180c924ac48cdee403
Xenon Zenon <j5yson5b5d@gmail.com>
2023-03-14 01:46:59 +0800

    0.0.4

--- sapot.py
+++ sapot.py
@@ -1,27 +1,16 @@
-# WSGI server
-from wsgiref.simple_server import make_server
+from http.server import HTTPServer
+from backend.sapotwebserver import Sapot
 
-def application(environ, start_response):
-    response_body = [
-       '%s: %s' % (key, value) for key, value in sorted(environ.items())
-    ]
-    response_body = '\n'.join(response_body)
-    response_body = [
-        'The Beginning\n',
-        '*' * 30 + '\n',
-        response_body,
-        '\n' + '*' * 30,
-        '\nThe End'
-    ]
-    response_body = [body.encode('utf-8') for body in response_body]
-    content_length = sum([len(s) for s in response_body])
-    status = '200 ok'
-    response_headers = [
-        ('Content-Type', 'text/plain'),
-        ('Content-Length', str(content_length))
-    ]
-    start_response(status, response_headers)
-    return response_body
+hostname = "localhost"
+port = 8000
 
-httpd = make_server('localhost', 8051, application)
-httpd.handle_request()
+if __name__ == "__main__":
+    sapot = HTTPServer((hostname, port), Sapot)
+    print("Sapot started http://%s:%s" % (hostname, port))
+
+    try:
+        sapot.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    sapot.server_close()
+    print("Sapot stopped.")
